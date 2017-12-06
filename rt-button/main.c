dc4ab0f34a6ecdc7e2c3977f45807d72536b1ee3
Weihong Guan <weihong.guan@gmail.com>
2017-12-06 16:48:29 +0800

    close unused hal in blinky and test with button in rt-button

--- rt-button/main.c
+++ rt-button/main.c
@@ -16,16 +16,41 @@
 
 #include "hal.h"
 #include "ch.h"
+#include "ch_test.h"
+/*
+ * Blinker thread #1.
+ */
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg) {
+  (void)arg;
+  chRegSetThreadName("blinker");
+
+  palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_OPENDRAIN);
+  while (true) {
+    palSetPad(GPIOC, GPIOC_LED);
+    chThdSleepMilliseconds(250);
+    palClearPad(GPIOC, GPIOC_LED);
+    chThdSleepMilliseconds(250);
+  }
+}
 
 int main(void) {
     halInit();
     chSysInit();
 
-    palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_OPENDRAIN);
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1, Thread1, NULL);
 
-    while(1) {
-        palTogglePad(GPIOC, GPIOC_LED);
-        chThdSleepMilliseconds(1000);
+    sdStart(&SD1, NULL);
+    palSetPadMode(GPIOA, 9, PAL_MODE_STM32_ALTERNATE_OPENDRAIN);       /* USART1 TX.       */
+    palSetPadMode(GPIOA, 10, PAL_MODE_INPUT);      /* USART1 RX.       */
+
+    palSetPadMode(GPIOB, 5, PAL_MODE_INPUT_PULLUP);
+
+    while (true) {
+      if (palReadPad(GPIOB, 5) == 0) {
+        test_execute((BaseSequentialStream *)&SD1);
+      }
+      chThdSleepMilliseconds(500);
     }
     return 0;
 }

551c72b054791d954f66c960c578be4d3c51abdc
Weihong Guan <weihong.guan@gmail.com>
2017-12-12 00:45:18 +0800

    init rt-SerialToI2c from rt-blinky in thread

---
+++ rt-SerialToI2c/main.c
@@ -0,0 +1,41 @@
+/*
+    ChibiOS - Copyright (C) 2006..2016 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "hal.h"
+#include "ch.h"
+
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg) {
+  (void)arg;
+  chRegSetThreadName("blinker");
+
+  palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_OPENDRAIN);
+  while (true) {
+    palTogglePad(GPIOC, GPIOC_LED);
+    chThdSleepMilliseconds(250);
+  }
+}
+
+int main(void) {
+    halInit();
+    chSysInit();
+
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, NULL);
+
+    while (true) {
+    }
+    return 0;
+}

d408e33dd1cb4757b15fd95b5f530f934b00637e
Weihong Guan <weihong.guan@gmail.com>
2017-12-14 14:49:53 +0800

    free/duty mailboxs

--- rt-SerialToI2c/main.c
+++ rt-SerialToI2c/main.c
@@ -1,14 +1,19 @@
 #include "hal.h"
 #include "ch.h"
+#include <string.h>
 
-#define MB_SIZE 8
+#define MB_SIZE 4
 #define BUFF_SIZE 256
 
 static uint8_t buff_rx[MB_SIZE][BUFF_SIZE];
-static uint8_t length_rx[MB_SIZE];
-static msg_t mb1_buffer[MB_SIZE];
-static MAILBOX_DECL(mb1, mb1_buffer, MB_SIZE);
-// static systime_t timeout = MS2ST(4);
+
+static msg_t mbfree_buffer[MB_SIZE];
+static MAILBOX_DECL(mbfree, mbfree_buffer, MB_SIZE);
+
+static msg_t mbduty_buffer[MB_SIZE];
+static MAILBOX_DECL(mbduty, mbduty_buffer, MB_SIZE);
+
+static systime_t timeout = MS2ST(200);
 
 static THD_WORKING_AREA(waBlink, 0);
 static THD_FUNCTION(Blink, arg) {
@@ -22,38 +27,46 @@ static THD_FUNCTION(Blink, arg) {
   }
 }
 
-static THD_WORKING_AREA(waPing, 64);
+static uint8_t buff[BUFF_SIZE];
+
+static THD_WORKING_AREA(waPing, 256);
 static THD_FUNCTION(Ping, arg) {
   (void)arg;
-
   chRegSetThreadName("ping");
-  msg_t i = 0;
+
+  uint8_t length;
+  uint8_t *p;
+  msg_t status;
 
   while (true) {
-    sdRead(&SD1, length_rx + i, 1);
-    if (length_rx[i]) {
-      // uint8_t len = sdReadTimeout(&SD1, *(buff_rx + i), length_rx[i], TIME_INFINITE);
-      uint8_t len = sdRead(&SD1, *(buff_rx + i), length_rx[i]);
-      if (len == length_rx[i]) {
-        chMBPost(&mb1, i, TIME_INFINITE);
-        if (++i == MB_SIZE) {
-          i = 0;
+    uint8_t len = sdReadTimeout(&SD1, buff, 1, timeout);
+    length = buff[0];
+    if (len == 1 && length > 0) {
+      len = sdReadTimeout(&SD1, buff + 1, length, timeout);
+      if (len == length) {
+        status = chMBFetch(&mbfree, (msg_t *)&p, TIME_IMMEDIATE);
+        if (status == MSG_OK) {
+          memcpy(p, buff, length);
+          chMBPost(&mbduty, (msg_t)p, TIME_INFINITE);
         }
       }
     }
   }
 }
 
-static THD_WORKING_AREA(waPong, 64);
+static THD_WORKING_AREA(waPong, 128);
 static THD_FUNCTION(Pong, arg) {
   (void)arg;
-  msg_t i;
+  void *p;
+  uint8_t length;
   chRegSetThreadName("pong");
 
   while (true) {
-    chMBFetch(&mb1, &i, TIME_INFINITE);
-    sdWrite(&SD1, length_rx + i, 1);
-    sdWrite(&SD1, *(buff_rx + i), length_rx[i]);
+    chMBFetch(&mbduty, (msg_t *)&p, TIME_INFINITE);
+    chThdSleepMilliseconds(20);
+    length = *(uint8_t *)p;
+    sdWrite(&SD1, p, length + 1);
+    chMBPost(&mbfree, (msg_t)p, TIME_INFINITE);
   }
 }
 
@@ -61,7 +74,11 @@ int main(void) {
     halInit();
     chSysInit();
 
-    chMBObjectInit(&mb1, mb1_buffer, MB_SIZE);
+    chMBObjectInit(&mbfree, mbfree_buffer, MB_SIZE);
+    chMBObjectInit(&mbduty, mbduty_buffer, MB_SIZE);
+
+    for (uint8_t i = 0; i < MB_SIZE; i++)
+      chMBPost(&mbfree, (msg_t)(*(buff_rx + i)), TIME_INFINITE);
 
     sdStart(&SD1, NULL);
     palSetPadMode(GPIOA, 9, PAL_MODE_STM32_ALTERNATE_OPENDRAIN);       // USART1 TX

37bb6bf51bfc35c016b5bdf1cd4ca7adf9569773
Weihong Guan <weihong.guan@gmail.com>
2017-12-18 02:32:19 +0800

    init smooth criminal

--- py/otto.py
+++ py/otto.py
@@ -1,12 +1,37 @@
 import serial
 import time
 from struct import pack, unpack
+from servo_daemon import ServoDaemon
+import easing
 
+
+class OttoServo():
+    def __init__(self, host, name, pin, home, minimun, maximun):
+        self.host = host
+        self.name = name
+        self.pin = pin
+        self.home = home
+        self.minimun = minimun
+        self.maximun = maximun
+
+    def get_width(self, angle):
+        if angle >= 90:
+            return int(self.home + (angle - 90.) * self.maximun / 90.)
+        return int(self.home + (90. - angle) * self.minimun / 90.)
+
+    def SetPosition(self, angle, seconds=0, func=easing.Linear):
+        width = self.get_width(angle)
+        self.host.curve(self.pin, width, seconds, func)
 
 YL = 0
 YR = 1
 RL = 2
 RR = 3
+
+# servo[0].attach(PIN_RR);
+# servo[1].attach(PIN_RL);
+# servo[2].attach(PIN_YR);
+# servo[3].attach(PIN_YL);
 
 # RL = 0
 # YL = 1
@@ -21,72 +46,245 @@ minimun = [-170, -178, -187, -172]
 maximun = [185, 224, 188, 203]
 
 
-
-tty = serial.Serial('/dev/tty.SLAB_USBtoUART', 115200, timeout=1)
-print(tty.name)
-
-
-def home():
-    for i in range(4):
-        send_width(i, homes[i])
-
-def set_angle(index, angle):
-    if angle >= 90:
-        send_width(index, int(homes[index] + (angle - 90.) * maximun[index] / 90.))
-    else:
-        send_width(index, int(homes[index] + (90. - angle) * minimun[index] / 90.))
-
-
-def send_width(index, width):
-    tty.write(pack('B', 4) + pack('B', index) + pack('<H', width))
-    print(tty.readline().decode())
-
-
-def set_all(lst):
-    for index, differ in enumerate(lst[:4]):
-        set_angle(index, 90 + differ)
-
-
-def updown():
-    set_all([0, 0, 45, -45])
-    time.sleep(2)
-    set_all([0, 0, 0, 0])
-    time.sleep(2)
+def delay(millis):
+    time.sleep(millis * 0.001)
 
 
 if __name__ == '__main__':
 
-    # set_angle(3, 0)
-    # time.sleep(4)
-    # set_angle(3, 180)
-    # time.sleep(4)
+    tty = serial.Serial('/dev/tty.SLAB_USBtoUART', 115200)
+    host = ServoDaemon(tty)
+    servo = [
+        OttoServo(host, 'RR', 3, 289, -172, 203),
+        OttoServo(host, 'RL', 2, 321, -187, 188),
+        OttoServo(host, 'YR', 1, 288, -178, 224),
+        OttoServo(host, 'YL', 0, 291, -170, 185),
+    ]
+    host.start()
+    host.connect()
 
-    home()
-    tempo = 0.495;
+    t = 495
+    # tmp = 0.495
 
-    # updown()
-    set_angle(3, 80)
-    set_angle(2, 100)
-    time.sleep(tempo)
-    set_angle(3, 70)
-    set_angle(2, 110)
-    time.sleep(tempo)
-    set_angle(3, 60)
-    set_angle(2, 120)
-    time.sleep(tempo)
-    set_angle(3, 50)
-    set_angle(2, 130)
-    time.sleep(tempo)
-    set_angle(3, 40)
-    set_angle(2, 140)
-    time.sleep(tempo)
-    set_angle(3, 30)
-    set_angle(2, 150)
-    time.sleep(tempo)
-    set_angle(3, 20)
-    set_angle(2, 160)
-    time.sleep(tempo)
+    def home():
+        for x in servo:
+            x.SetPosition(90)
 
-    home()
+    def goingUp(tempo):
+        home()
+        delay(tempo)
+        servo[0].SetPosition(80)
+        servo[1].SetPosition(100)
+        delay(tempo)
+        servo[0].SetPosition(70)
+        servo[1].SetPosition(110)
+        delay(tempo)
+        servo[0].SetPosition(60)
+        servo[1].SetPosition(120)
+        delay(tempo)
+        servo[0].SetPosition(50)
+        servo[1].SetPosition(130)
+        delay(tempo)
+        servo[0].SetPosition(40)
+        servo[1].SetPosition(140)
+        delay(tempo)
+        servo[0].SetPosition(30)
+        servo[1].SetPosition(150)
+        delay(tempo)
+        servo[0].SetPosition(20)
+        servo[1].SetPosition(160)
+        delay(tempo)
 
-    tty.close()
+    def kickLeft(tempo):
+        home()
+        delay(tempo)
+        servo[0].SetPosition(50)
+        servo[1].SetPosition(70)
+        delay(tempo)
+        servo[0].SetPosition(80)
+        servo[1].SetPosition(70)
+        delay(tempo/4)
+        servo[0].SetPosition(30)
+        servo[1].SetPosition(70)
+        delay(tempo/4)
+        servo[0].SetPosition(80)
+        servo[1].SetPosition(70)
+        delay(tempo/4)
+        servo[0].SetPosition(30)
+        servo[1].SetPosition(70)
+        delay(tempo/4)
+        servo[0].SetPosition(80)
+        servo[1].SetPosition(70)
+        delay(tempo)
+
+    def kickRight(tempo):
+        home();
+        delay(tempo)
+        servo[0].SetPosition(110)
+        servo[1].SetPosition(130)
+        delay(tempo)
+        servo[0].SetPosition(110)
+        servo[1].SetPosition(100)
+        delay(tempo/4)
+        servo[0].SetPosition(110)
+        servo[1].SetPosition(150)
+        delay(tempo/4)
+        servo[0].SetPosition(110)
+        servo[1].SetPosition(80)
+        delay(tempo/4)
+        servo[0].SetPosition(110)
+        servo[1].SetPosition(150)
+        delay(tempo/4)
+        servo[0].SetPosition(110)
+        servo[1].SetPosition(100)
+        delay(tempo)
+
+    def lateral_fuerte(side, tempo):
+        home()
+        if side:
+            servo[0].SetPosition(40)
+        else:
+            servo[1].SetPosition(140)
+
+        delay(tempo/2)
+
+        servo[0].SetPosition(90)
+        servo[1].SetPosition(90)
+        delay(tempo/2)
+
+    def moveNServos(time, newPosition):
+        for i, x in enumerate(newPosition):
+            servo[i].SetPosition(x)
+        delay(time)
+
+    def primera_parte():
+        move1 = [60, 120, 90, 90]
+        move2 = [90, 90, 90, 90]
+        move3 = [40, 140, 90, 90]
+
+        for _ in range(3):
+            for _ in range(3):
+              lateral_fuerte(1, t/2);
+              lateral_fuerte(0, t/4);
+              lateral_fuerte(1, t/4);
+
+
+
+        # pause=millis();
+        home()
+        moveNServos(t*0.4, move1);
+        moveNServos(t*0.4, move2);
+        delay(t * 1.2)
+
+        # while(millis()<(pause+t*2));
+        # }
+        #
+        for _ in range(2):
+            lateral_fuerte(1, t/2)
+            lateral_fuerte(0, t/4)
+            lateral_fuerte(1, t/4)
+            delay(t)
+        # }
+        #
+        # pause=millis();
+        home()
+        # crusaito(1, t*1.4);
+        moveNServos(t*1, move3)
+        delay(2*t)
+        home()
+        # while(millis()<(pause+t*4));
+        delay(2 * t)
+
+    try:
+        home()
+        # goingUp()
+        kickLeft(t)
+        kickRight(t)
+
+        primera_parte()
+
+        home()
+        delay(1)
+
+        # t.curve(15, 150, 0, easing.Linear)
+        # time.sleep(1)
+        # # while True:
+        # t.curve(15, 450, 1.5, easing.BounceEaseOut)
+        # time.sleep(1.6)
+        # t.curve(15, 150, 1.5, easing.BounceEaseOut)
+        # time.sleep(1.6)
+
+    except (KeyboardInterrupt, SystemExit):
+        print('exit')
+    finally:
+        host.stop()
+        host.join()
+
+# tty = serial.Serial('/dev/tty.SLAB_USBtoUART', 115200)
+# print(tty.name)
+#
+#
+# def home():
+#     for i in range(4):
+#         send_width(i, homes[i])
+#
+# def set_angle(index, angle):
+#     if angle >= 90:
+#         send_width(index, int(homes[index] + (angle - 90.) * maximun[index] / 90.))
+#     else:
+#         send_width(index, int(homes[index] + (90. - angle) * minimun[index] / 90.))
+#
+#
+# def send_width(index, width):
+#     tty.write(pack('B', 4) + pack('B', index) + pack('<H', width))
+#     print(tty.readline().decode())
+#
+#
+# def set_all(lst):
+#     for index, differ in enumerate(lst[:4]):
+#         set_angle(index, 90 + differ)
+#
+#
+# def updown():
+#     set_all([0, 0, 45, -45])
+#     time.sleep(2)
+#     set_all([0, 0, 0, 0])
+#     time.sleep(2)
+#
+#
+# if __name__ == '__main__':
+#
+#     # set_angle(3, 0)
+#     # time.sleep(4)
+#     # set_angle(3, 180)
+#     # time.sleep(4)
+#
+#     home()
+#     tempo = 0.495;
+#
+#     # updown()
+#     set_angle(3, 80)
+#     set_angle(2, 100)
+#     time.sleep(tempo)
+#     set_angle(3, 70)
+#     set_angle(2, 110)
+#     time.sleep(tempo)
+#     set_angle(3, 60)
+#     set_angle(2, 120)
+#     time.sleep(tempo)
+#     set_angle(3, 50)
+#     set_angle(2, 130)
+#     time.sleep(tempo)
+#     set_angle(3, 40)
+#     set_angle(2, 140)
+#     time.sleep(tempo)
+#     set_angle(3, 30)
+#     set_angle(2, 150)
+#     time.sleep(tempo)
+#     set_angle(3, 20)
+#     set_angle(2, 160)
+#     time.sleep(tempo)
+#
+#     home()
+#
+#     tty.close()
--- py/servo_daemon.py
+++ py/servo_daemon.py
@@ -1,7 +1,103 @@
 import threading
+from serial.threaded import Protocol
 import time
+import serial
+from struct import unpack
 import easing
 from datetime import datetime
+from functools import reduce
+
+
+def toHex(bs):
+    return ' '.join('%02x' % c for c in bs)
+
+
+class RecvProtocol(Protocol):
+    def __init__(self):
+        self.packet = bytearray()
+        self.in_packet = False
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def connection_lost(self, exc):
+        self.transport = None
+        self.in_packet = False
+        self.pointer = 0
+        self.length = 0
+        del self.packet[:]
+        super(RecvProtocol, self).connection_lost(exc)
+
+    def data_received(self, data):
+        # print(toHex(data))
+        for byte in serial.iterbytes(data):
+            if self.in_packet:
+                self.packet.extend(byte)
+                self.pointer += 1
+            else:
+                self.length = unpack('B', byte)[0]
+                self.packet.extend(byte)
+                self.pointer = 0
+                self.in_packet = True
+
+            if self.pointer == self.length:
+                self.in_packet = False
+                self.handle_packet(bytes(self.packet))  # make read-only copy
+                del self.packet[:]
+
+    def handle_packet(self, packet):
+        pass
+        # print('rx:', toHex(packet), datetime.now())
+        # print('rx:', toHex(packet))
+
+
+class BusDaemon(threading.Thread):
+    def __init__(self, tty, recv_protocol):
+        super(BusDaemon, self).__init__()
+        self.tty = tty
+        self.receiver = recv_protocol()
+        self.alive = False
+        self._lock = threading.Lock()
+        self._connected = threading.Event()
+        self.uid = 0
+
+    def stop(self):
+        self.alive = False
+
+    def run(self):
+        self.tty.timeout = 1
+        self._connected.wait()
+
+        error = None
+        while self.alive and self.tty.is_open:
+            try:
+                data = self.tty.read(self.tty.in_waiting or 1)
+            except serial.SerialException as e:
+                error = e
+                break
+            else:
+                if data:
+                    self.receiver.data_received(data)
+
+        self.alive = False
+        self.receiver.connection_lost(error)
+        self.receiver = None
+        self.tty.close()
+
+    def connect(self):
+        self.receiver.connection_made(self)
+        self.alive = True
+        self._connected.set()
+
+    def write(self, data):
+        with self._lock:
+            tx = bytes([len(data) + 2, self.uid, 0xff - self.uid]) + data
+            # print('tx:', toHex(tx), datetime.now())
+            print('tx:', toHex(tx))
+            self.tty.write(tx)
+            self.uid += 1
+            self.uid &= 0xff
 
 
 class Servo():
@@ -13,64 +109,88 @@ class Servo():
 
     def update(self):
         if self.step < self.span:
-            self.position = self.func(self.step + 1)
+            self.position = int(self.func(self.step + 1))
             self.step += 1
 
-        if self.step == self.span:
+        if self.step >= self.span:
             self.func = None
             self.step = 0
             self.span = 0
 
-    def curve(self, easing_factory, to, span):
-        self.func = easing_factory(self.position, to, span).ease
+    def curve(self, to, span, func):
+        self.func = func(self.position, to, span).ease
         self.span = span
 
 
-class Bus(threading.Thread):
-    def __init__(self, length):
-        super(Bus, self).__init__()
+class ServoDaemon(threading.Thread):
+    def __init__(self, tty, length=16, freq=100, downtime=0.002):
+        super(ServoDaemon, self).__init__()
         self.length = length
-        self.servos = [Servo(0.) for _ in range(self.length)]
+        self.servos = [Servo(300) for _ in range(self.length)]
         self.alive = True
-        self.scale = 10
-        self.sleep = 1.0 / self.scale - 0.005
+        self.bus = BusDaemon(tty, RecvProtocol)
+        self.freq = freq
+        self.sleep = 1.0 / self.freq - downtime
+        self._connected = threading.Event()
 
     def update(self):
         for x in self.servos:
             x.update()
 
+    def connect(self):
+        self.bus.start()
+        self.bus.connect()
+        self.bus.write(bytes([0x40, 0, 0] + [0, 0x31]))
+        self.bus.write(bytes([0x40, 0, 0] + [0xfe, 136]))
+        self.bus.write(bytes([0x40, 0, 0] + [0, 0x21]))
+        self.transmit()
+        time.sleep(1)
+        self._connected.set()
+
+    def transmit(self):
+        vals = [[0, 0, x.position & 0xff, x.position >> 8] for x in self.servos]
+        payload = reduce(lambda c, x: c + x, vals, [])
+        self.bus.write(bytes([0x40, 0, 0] + [6] + payload))
+
     def run(self):
-        print(' '.join(['%+2.4f' % x.position for x in self.servos]))
+        self._connected.wait()
 
         while self.alive:
-            if [x.span for x in self.servos] != [0] * self.length:
-                print(' '.join('%d/%d' % (x.step, x.span) for x in self.servos))
+            if any([x.span for x in self.servos]):
+                # print(' '.join('%d/%d' % (x.step, x.span) for x in self.servos))
                 self.update()
-                print(datetime.now().strftime('%H:%M:%S.%f'), ' '.join(['%+2.4f' % x.position for x in self.servos]))
+                self.transmit()
+                # print(datetime.now().strftime('%H:%M:%S.%f'), ' '.join(['%03d' % x.position for x in self.servos]))
             time.sleep(self.sleep)
 
     def stop(self):
         self.alive = False
-        self.join()
+        self.bus.stop()
+        self.bus.join()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop()
-
-    def curve(self, index, func, to, seconds):
-        self.servos[index].curve(func, to, seconds * self.scale)
+    def curve(self, index, to, seconds=0, func=easing.Linear):
+        if seconds == 0:
+            self.servos[index].curve(to, 1, func)
+        else:
+            self.servos[index].curve(to, int(seconds * self.freq), func)
 
 
 if __name__ == '__main__':
-    t = Bus(4)
     try:
+        tty = serial.Serial('/dev/tty.SLAB_USBtoUART', 115200)
+        t = ServoDaemon(tty)
         t.start()
-        time.sleep(0.2)
-        t.curve(1, easing.Linear, 4, 2)
-        time.sleep(0.3)
-        t.curve(2, easing.ElasticEaseIn, 3, 2)
+        t.connect()
+        t.curve(15, 150, 0)
+        time.sleep(1)
+        # while True:
+        t.curve(15, 450, 1.5)
+        time.sleep(1.6)
+        t.curve(15, 150, 1.5)
+        time.sleep(1.6)
 
-        time.sleep(4)
     except (KeyboardInterrupt, SystemExit):
         print('exit')
     finally:
         t.stop()
+        t.join()

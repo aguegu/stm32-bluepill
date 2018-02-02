43fdf97cae626f4d9254789bd5a7b93d1d59e0b4
Weihong Guan <weihong.guan@gmail.com>
2018-02-03 00:41:47 +0800

    ssd1306

--- rt-otto2/main.c
+++ rt-otto2/main.c
@@ -1,20 +1,23 @@
 #include "hal.h"
-#include "ch.h"
-#include <string.h>
+#include "string.h"
+#include "stdlib.h"
 
-#define MB_SIZE 4
-#define BUFF_SIZE 256
+#define WIDTH		128
+#define HEIGHT  32
+#define LEN (WIDTH * HEIGHT >> 3)
+#define LEN_RAW (LEN + 3)
 
-static uint8_t buff_rx[MB_SIZE][BUFF_SIZE];
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg) {
+  (void)arg;
+  chRegSetThreadName("blinker");
 
-static msg_t mbfree_buffer[MB_SIZE];
-static MAILBOX_DECL(mbfree, mbfree_buffer, MB_SIZE);
-
-static msg_t mbduty_buffer[MB_SIZE];
-static MAILBOX_DECL(mbduty, mbduty_buffer, MB_SIZE);
-
-static systime_t timeout = MS2ST(200);
-static mutex_t mtx_sd1;
+  palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_OPENDRAIN);
+  while (true) {
+    palTogglePad(GPIOC, GPIOC_LED);
+    chThdSleepMilliseconds(500);
+  }
+}
 
 static const I2CConfig i2cfg = {
     OPMODE_I2C,
@@ -22,137 +25,129 @@ static const I2CConfig i2cfg = {
     FAST_DUTY_CYCLE_2,
 };
 
-static THD_WORKING_AREA(waBlink, 0);
-static THD_FUNCTION(Blink, arg) {
-  (void)arg;
-  chRegSetThreadName("blink");
+static const uint8_t font[30] = {
+  0x3f, 0x21, 0x3f,
+  0x22, 0x3f, 0x20,
+  0x3d, 0x25, 0x27,
+  0x25, 0x25, 0x3f,
+  0x0f, 0x08, 0x3f,
+  0x27, 0x25, 0x3d,
+  0x3f, 0x25, 0x3d,
+  0x01, 0x01, 0x3f,
+  0x3f, 0x25, 0x3f,
+  0x2f, 0x29, 0x3f,
+};
 
-  palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_OPENDRAIN);
-  while (true) {
-    palTogglePad(GPIOC, GPIOC_LED);
-    chThdSleepMilliseconds(250);
-  }
+void init(I2CDriver *i2cp) {
+  msg_t status = MSG_OK;
+
+  i2cAcquireBus(i2cp);
+
+  uint8_t s[27] = {0x00, 0xae, 0xd5, 0x80, 0xa8, 31, 0x40, 0x8d, 0x14, 0x20, 0x00, 0xa0, 0xc0, 0xda, 0x02, 0x81, 0x8f, 0xd9, 0xf1, 0xdb, 0x40, 0xa4, 0xa6, 0x2e, 0xaf, 0x00, 0x10};
+
+  status = i2cMasterTransmit(i2cp, 0x3c,
+                          s, 27, NULL, 0);
+  osalDbgCheck(MSG_OK == status);
+  i2cReleaseBus(i2cp);
 }
 
-static THD_WORKING_AREA(waPing, 512);
-static THD_FUNCTION(Ping, arg) {
-  (void)arg;
-  chRegSetThreadName("ping");
-  uint8_t buff[BUFF_SIZE];
-  uint8_t rx[4];;
-  uint8_t length;
-  uint8_t *p;
-  msg_t status;
+uint8_t raw[LEN_RAW];
+uint8_t *buffer = raw + 3;
 
-  while (true) {
-    sdRead(&SD1, buff, 1);
-    length = buff[0];
-    if (length >= 2) {
-      sdRead(&SD1, buff + 1, 2);
-      if (buff[1] + buff[2] == 0xff) {
-        if (length == 2) {
-          chMtxLock(&mtx_sd1);
-          sdWrite(&SD1, buff, 3);
-          chMtxUnlock(&mtx_sd1);
-        } else {
-          uint8_t len = sdReadTimeout(&SD1, buff + 3, length - 2, timeout);
-          if (len == length - 2) {
-            status = chMBFetch(&mbfree, (msg_t *)&p, TIME_IMMEDIATE);
-            if (status == MSG_OK) {
-              memcpy(p, buff, length + 1);
-              chMBPost(&mbduty, (msg_t)p, TIME_INFINITE);
-            } else {
-              chMtxLock(&mtx_sd1);
-              rx[0] = 3;
-              rx[1] = buff[1];
-              rx[2] = buff[2];
-              rx[3] = 0xff;
-              sdWrite(&SD1, rx, 4); // busy response
-              chMtxUnlock(&mtx_sd1);
-            }
-          }
-        }
-      }
-    }
-  }
+void display(I2CDriver *i2cp) {
+  i2cMasterTransmit(i2cp, 0x3c,
+                        raw, LEN_RAW, NULL, 0);
+  i2cReleaseBus(i2cp);
 }
 
-static THD_WORKING_AREA(waPong, 1024);
-static THD_FUNCTION(Pong, arg) {
-  (void)arg;
-  uint8_t *p;
-  chRegSetThreadName("pong");
-  uint8_t buff[BUFF_SIZE];
-  systime_t tmo = MS2ST(4);
-  msg_t status;
+void clear(void) {
+  memset(buffer, 0x00, LEN);
+}
 
-  while (true) {
-    chMBFetch(&mbduty, (msg_t *)&p, TIME_INFINITE);
-    memcpy(buff, p, 3);
-    i2cAcquireBus(&I2CD2);
+void plot(uint8_t x, uint8_t y) {
+  buffer[(y >> 3) * WIDTH + x] |= 1 << (y & 0x07);
+}
 
-    if (p[0] == 5) {
-      status = i2cMasterReceiveTimeout(&I2CD2, p[3], buff + 4, p[5], tmo);
+void line(int16_t x0, int16_t y0, int16_t x1, int16_t y1) {
+  int16_t steep = abs(y1 - y0) > abs(x1 - x0);
+  int16_t tmp;
+
+  if (steep) {
+    tmp = x0; x0 = y0; y0 = tmp;
+    tmp = x1; x1 = y1; y1 = tmp;
+  }
+
+  if (x0 > x1) {
+    tmp = x0; x0 = x1; x1 = tmp;
+    tmp = y0; y0 = y1; y1 = tmp;
+  }
+
+  int16_t dx, dy;
+  dx = x1 - x0;
+  dy = abs(y1 - y0);
+  int16_t err = dx >> 1;
+  int16_t ystep = y0 < y1 ? 1 : -1;
+
+  for (; x0<=x1; x0++) {
+    if (steep) {
+      plot(y0, x0);
     } else {
-      status = i2cMasterTransmitTimeout(&I2CD2, p[3],
-                              p + 6, p[0] - 5, buff + 4, p[5], tmo);
+      plot(x0, y0);
     }
-
-    if (p[4]) {
-      chThdSleepMilliseconds(p[4]);
+    err -= dy;
+    if (err < 0) {
+        y0 += ystep;
+        err += dx;
     }
-
-    switch (status) {
-      case MSG_TIMEOUT:
-        buff[3] = 1;
-        buff[0] = 3;
-        break;
-      case MSG_RESET:
-        buff[3] = 2;
-        buff[0] = 3;
-        break;
-      default:
-        buff[3] = status;
-        buff[0] = p[5] + 3;
-    }
-
-    i2cReleaseBus(&I2CD2);
-
-    chMtxLock(&mtx_sd1);
-
-    sdWrite(&SD1, buff, buff[0] + 1);
-    chMtxUnlock(&mtx_sd1);
-
-    chMBPost(&mbfree, (msg_t)p, TIME_INFINITE);
   }
+}
+
+void frame(void) {
+  for (uint16_t i=0; i<8; i++) {
+    uint16_t offset = (i << 6) + (i & 0x01) * 28;
+    memcpy(buffer + offset, font + i * 3, 3);
+    buffer[offset + 5] = 0x28;
+  }
+}
+
+void printVal(uint16_t index, uint16_t val) {
+  uint8_t st = 32;
+  uint16_t offset = (index << 6) + (index & 0x01) * 28;
+  do {
+    uint8_t x = val % 10;
+    memcpy(buffer + offset + st, font + x * 3, 3);
+    val /= 10;
+    st -= 4;
+  } while(val);
 }
 
 int main(void) {
     halInit();
     chSysInit();
 
-    chMBObjectInit(&mbfree, mbfree_buffer, MB_SIZE);
-    chMBObjectInit(&mbduty, mbduty_buffer, MB_SIZE);
-
-    chMtxObjectInit(&mtx_sd1);
-
-    for (uint8_t i = 0; i < MB_SIZE; i++)
-      chMBPost(&mbfree, (msg_t)(*(buff_rx + i)), TIME_INFINITE);
-
-    sdStart(&SD1, NULL);
-    palSetPadMode(GPIOA, 9, PAL_MODE_STM32_ALTERNATE_OPENDRAIN);       // USART1 TX
-    palSetPadMode(GPIOA, 10, PAL_MODE_INPUT);                          // USART1 RX
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1, Thread1, NULL);
 
     i2cStart(&I2CD2, &i2cfg);
     palSetPadMode(GPIOB, 10, PAL_MODE_STM32_ALTERNATE_OPENDRAIN);  // I2C SCL
     palSetPadMode(GPIOB, 11, PAL_MODE_STM32_ALTERNATE_OPENDRAIN);  // I2C SDA
 
-    chThdCreateStatic(waBlink, sizeof(waBlink), (NORMALPRIO + 1), Blink, NULL);
-    chThdCreateStatic(waPing, sizeof(waPing), (NORMALPRIO), Ping, NULL);
-    chThdCreateStatic(waPong, sizeof(waPong), (NORMALPRIO), Pong, NULL);
+    init(&I2CD2);
+    raw[0] = 0x80;
+    raw[1] = 0xb0;
+    raw[2] = 0x40;
+
+    clear();
+    frame();
+
+    for (uint8_t i=0; i<8; i++) {
+      printVal(i, i * 7313);
+    }
+
+    display(&I2CD2);
 
     while (true) {
-      chThdSleepSeconds(1);
+      chThdSleepMilliseconds(500);
+
     }
     return 0;
 }

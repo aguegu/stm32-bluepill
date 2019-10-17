a6656b266de98ba7e280cc0fa804d74a786b7727
Weihong Guan <weihong.guan@gmail.com>
2019-10-17 13:01:52 +0800

    remove demos before adept to 19.1.3

--- utils/nil/utils.mk
+++
@@ -1,7 +0,0 @@
-# List of all the ChibiOS/NIL test files.
-UTILSRC = $(UTILS)/nil/i2c.c \
-					$(UTILS)/nil/ds3231.c \
-					$(UTILS)/nil/pca9685.c \
-
-# Required include directories
-UTILINC = $(UTILS)/nil
